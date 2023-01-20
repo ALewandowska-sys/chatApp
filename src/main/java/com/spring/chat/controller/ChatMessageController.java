c64176b9278661dd648ebd9ecc51b6fb4b536f16
ALewandowska-sys <ola.lewa109@gmail.com>
2023-01-20 20:56:13 +0100

    add properties

--- src/main/java/com/spring/chat/controller/ChatMessageController.java
+++ src/main/java/com/spring/chat/controller/ChatMessageController.java
@@ -9,16 +9,9 @@ import org.springframework.stereotype.Controller;
 public class ChatMessageController {
 
     @MessageMapping("/chat")
-    //moze byc kolejka dla kazdego uzytkownia inna i zamiast messages to bedzie polaczenie
+    //dla kazdego uzytkownia inna i zamiast messages to bedzie polaczenie
     @SendTo("/topic/messages")
     public ChatMessage get(ChatMessage chatMessage) {
-        return chatMessage;
-    }
-
-    @MessageMapping("/chat")
-    //moze byc kolejka dla kazdego uzytkownia inna i zamiast messages to bedzie polaczenie
-    @SendTo("/topic/messages2")
-    public ChatMessage get2(ChatMessage chatMessage) {
         return chatMessage;
     }
 
