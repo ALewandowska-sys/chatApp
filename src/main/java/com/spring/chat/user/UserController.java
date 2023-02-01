f30152f936efaa659e7a024f0d588d70947719cb
ALewandowska-sys <ola.lewa109@gmail.com>
2023-02-01 22:36:05 +0100

    create CRUD operations

---
+++ src/main/java/com/spring/chat/user/UserController.java
@@ -0,0 +1,49 @@
+package com.spring.chat.user;
+
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/testRepo")
+    public String testRepo(){
+        return userService.testRepo();
+    }
+
+    @GetMapping
+    public Page<UserDto> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/byName/{username}")
+    public UserDto getUserByName(@RequestParam(value = "username") String username){
+        return userService.getUserByName(username);
+    }
+
+    @GetMapping("/byId/{userId}")
+    public UserDto getUserById(@RequestParam(value = "userId") Long userId){
+        return userService.getUserById(userId);
+    }
+
+    @PostMapping
+    public UserDto saveUser(@RequestBody UserDto userDto){
+        return userService.saveUser(userDto);
+    }
+
+    @PutMapping("/{userId}")
+    public UserDto updateUser(@RequestBody UserDto userDto,
+                              @PathVariable("userId") Long userId){
+        return userService.updateUser(userId, userDto);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable("userId") Long userId){
+        userService.deleteUser(userId);
+    }
+}
--- src/main/java/com/spring/chat/user/UserService.java
+++ src/main/java/com/spring/chat/user/UserService.java
@@ -1,12 +1,71 @@
 package com.spring.chat.user;
 
+import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepo userRepo;
+    private final UserMapper userMapper = Mappers.getMapper(UserMapper.class);
 
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
+    }
+
+    public UserDto getUserByName(String username){
+        return userMapper.toUserDto(userRepo.findByUsername(username).orElse(null));
+    }
+
+    public UserDto getUserById(Long id) {
+        return userMapper.toUserDto(userRepo.findById(id).orElse(null));
+    }
+
+    public UserDto saveUser(UserDto userDto){
+        checkUsernameIsUnique(userDto.getUsername());
+        User newUser = userRepo.save(userMapper.toUserEntity(userDto));
+        return userMapper.toUserDto(newUser);
+    }
+
+    public UserDto updateUser(Long id, UserDto userDto){
+        Optional<User> userUpdate = userRepo.findById(id);
+        if(userUpdate.isEmpty()){
+            throw new RuntimeException("There is any user with id: " + id);
+        }
+        User user = userUpdate.get();
+        if(userDto.getUsername() != null){
+            checkUsernameIsUnique(userDto.getUsername());
+            user.setUsername(userDto.getUsername());
+        }
+        if(userDto.getPassword() != null){
+            user.setPassword(userDto.getPassword());
+        }
+        return userMapper.toUserDto(userRepo.save(user));
+    }
+
+    public void deleteUser(Long id){
+        Optional<User> userDelete = userRepo.findById(id);
+        userDelete.ifPresent(user -> userRepo.deleteById(id));
+    }
+
+    private void checkUsernameIsUnique(String username) {
+        Optional<User> user = userRepo.findByUsername(username);
+        if(user.isPresent()){
+            throw new RuntimeException("Username have to be unique");
+        }
+    }
+
+    public Page<UserDto> getUsers() {
+        Pageable pageable = PageRequest.of(0, 5, Sort.by("username"));
+        return userRepo.findAll(pageable).map(userMapper::toUserDto);
+    }
+
+    public String testRepo() {
+        return userRepo.findAll().toString();
     }
 }
