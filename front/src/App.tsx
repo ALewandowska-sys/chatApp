0237366b8b9fc7f6ce12a3b0e13a12b9b7c565f9
tomektomek321 <71927596+tomektomek321@users.noreply.github.com>
2023-02-14 06:34:34 +0100

    Basic page for logged user (#13)
    * Basic layout
    
    * small fixes
    
    * small fixes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PagesUserHome from './pages/user-home/PagesUserHome';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      App works!
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/myhome" element={<PagesUserHome />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ front/src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='container'>
+      <h1>
+        Empty Header :(
+      </h1>
+    </div>
+  )
+}
