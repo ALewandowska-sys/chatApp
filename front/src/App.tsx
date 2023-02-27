c72d9aae21fdd915fac0e0187bbefb358c512e7d
nikiDesigns <104830490+NNikiforuk@users.noreply.github.com>
2023-02-27 14:12:21 +0100

    Navbar added (#16)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PagesUserHome from './pages/user-home/PagesUserHome';
-import Header from './components/header/Header';
-import Register from './auth/register/Register';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PagesUserHome from "./pages/user-home/PagesUserHome";
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import Login from "./components/navbar/Login";
+import Logout from "./components/navbar/Logout";
+import Register from "./components/navbar/Register";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/myhome" element={<PagesUserHome />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+			<Navbar />
+			<Routes>
+				<Route path="/myhome" element={<PagesUserHome />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/logout" element={<Logout />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ front/src/components/navbar/Logout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Logout() {
+	return (
+		<div className="container">
+			<h1>Wyloguj się</h1>
+		</div>
+	);
+}
