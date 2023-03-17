de320def6625b050a4735a6e39403a202a47e3f0
Tomek <tomek1989gw@wp.pl>
2023-03-17 12:59:57 +0000

    Set common navbar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,10 +5,12 @@ import PagesUserHome from "./pages/user-home/PagesUserHome";
 import LandingPage from "./components/landing_page/LandingPage";
 import Login from "./components/navbar/Login";
 import Logout from "./components/navbar/Logout";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<LandingPage />} />
 				<Route path="/myhome" element={<PagesUserHome />} />
