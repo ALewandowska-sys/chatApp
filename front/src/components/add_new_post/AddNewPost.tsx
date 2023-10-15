04e9b389073a3b44e7ee42356d6b374137e5a747
Tomek <tomek1989gw@wp.pl>
2023-10-15 07:55:03 +0000

    fixes

--- front/src/components/add_new_post/AddNewPost.tsx
+++ front/src/components/add_new_post/AddNewPost.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./AddNewPost.scss";
 import { Timestamp } from "firebase/firestore";
 import { collection, addDoc } from "firebase/firestore";
@@ -14,56 +14,38 @@ interface IAddNewPostForm {
 }
 
 export default function AddNewPost() {
-  // const [content, setContent] = useState("");
-  const { register, formState, watch } = useForm<IAddNewPostForm>({
+  const { register, formState } = useForm<IAddNewPostForm>({
     mode: "onTouched",
   });
 
-  // Get the current user from Firebase Authentication
   const [user] = useAuthState(auth);
-
-  // Function to handle changes in the textarea input
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    // setContent(e.target.value);
-  };
 
   const onSubmit = (e: any) => {
     e.preventDefault();
     console.log(formState);
-    console.log(formState.errors);
-    // console.log(formState.dirtyFields);
-    // console.log(formState.isValid);
+    handlePublish();
   };
 
   // Function to handle publishing a new post
   const handlePublish = async () => {
-    console.log("publish click");
 
-    if (!true) {
-      alert("Post cannot be empty");
-      return;
-    } else {
-      if (user) {
-        console.log("if user is logged in");
+    if (user) {
+      // Prepare the new post data
+      const postCollectionRef = collection(firestore, "Posts");
+      const newPost = {
+        userId: user.uid,
+        content: "",// content,
+        createdAt: Timestamp.now().toDate(),
+        comments: [],
+        reactions: [], // Initialize an empty array to store reactions (will be updated later)
+      };
 
-        // Prepare the new post data
-        const postCollectionRef = collection(firestore, "Posts");
-        const newPost = {
-          userId: user.uid,
-          content: "",// content,
-          createdAt: Timestamp.now().toDate(),
-          comments: [],
-          reactions: [], // Initialize an empty array to store reactions (will be updated later)
-        };
-
-        try {
-          // Add the new post to the Firestore collection
-          await addDoc(postCollectionRef, newPost);
-          //setContent(""); // Reset the content state after successful publishing
-          console.log("Post published successfully!");
-        } catch (error) {
-          console.log("Error publishing the post:", error);
-        }
+      try {
+        // Add the new post to the Firestore collection
+        await addDoc(postCollectionRef, newPost);
+        console.log("Post published successfully!");
+      } catch (error) {
+        console.log("Error publishing the post:", error);
       }
     }
   };
@@ -71,7 +53,7 @@ export default function AddNewPost() {
   return (
     <div className="add-new-post__container container">
       <form className="add-new-post">
-        <h4>
+        <h4 className="add-new-post__labelWrapper">
           <label
             htmlFor="exampleFormControlTextarea1"
             className="add-new-post__label"
